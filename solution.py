eeecd9402851b72827dc2e88d20e6d3444b536c6
Aarif38 <abdullah38arif@gmail.com>
2026-02-11 19:25:26 +0300

    Add files via upload

--- solution.py
+++ solution.py
@@ -12,6 +12,24 @@ import numpy as np
 
 
 def solution(image, current_speed, current_steering):
+
+    grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurredImage= cv2.GaussianBlur(grayImage,(5, 5), 0)
+    sınır, rota = cv2.threshold(blurredImage, 60, 255, cv2.THRESH_BINARY_INV)
+    satır, sütun = rota.shape
+    önsatır = satır - 25
+    roi = rota[önsatır:satır, 0:sütun]
+    image_center = sütun/2
+    GaripBiSey = cv2.moments(rota)
+
+    if GaripBiSey['m00'] > 0:
+        yol = GaripBiSey['m10'] / GaripBiSey['m00']
+        steering = (yol - image_center) * 0.01
+    else: 
+        steering = current_steering
+    speed = 10
+    target_speed = speed 
+    steering = np.clip(steering, -1.0, 1.0)
     """  
     Args:
         image: Robotun kamerasından gelen 64x64 pixel BGR görüntü (numpy array)
